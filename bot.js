15f1eaf32039a19800245fc5a12a18add8a07e51
PPRhuM <45391407+PPRhuM@users.noreply.github.com>
2019-04-04 12:28:20 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+var generalChannel = false
+var germUserId = "555130004074135563" 
+var chanToUse = "559733838369128461"
+
+client.on('ready', () => {
+    console.log("Connected as " + client.user.tag)
+	generalChannel = client.channels.get(chanToUse)
+	generalChannel.send("J'arrive pour pourrir germ !!!") 
+})
+
+client.on('presenceUpdate', (oldMember, newMember) => {
+	if (generalChannel != false) {
+		if (oldMember.user.id == germUserId) {
+			generalChannel.send("Coucou Germain !!! https://www.youtube.com/watch?v=5CJ_fdEJy3Y") 
+		}
+	}
+})
+
+client.login(process.env.BOT_TOKEN)
